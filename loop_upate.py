b56edd279071f89399e1288ee902c2654a7be395
Armageddon-15 <hjc014@126.com>
2023-02-21 17:56:13 +0800

    Update loop_upate.py

--- loop_upate.py
+++ loop_upate.py
@@ -1,20 +1,23 @@
 import time
 import datetime
 import random
-import os
+from download_thbnl import update
 
 
 def loop():
     while True:
         print("\n---start---\n")
-        os.system("venv\\Scripts\\python.exe download_thbnl.py")
+        try:
+            update()
+        except Exception as e:
+            print(e)
         print("\n---end---\n")
         now = datetime.datetime.now()
-        # print("finish time : %d:%d:%d" % (now.tm_hour, now.tm_min, now.tm_sec))
         print("finish update at %s" % now)
         delay = random.randint(3400, 3600)
         print("next update at %s" % (now + datetime.timedelta(seconds=delay)))
         time.sleep(delay)
 
 
-loop()
+if __name__ == '__main__':
+    loop()
